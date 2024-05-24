90a57498bce6230e88516aaa37695a74e486a007
Happy <153811269+Muhtashim99@users.noreply.github.com>
2024-05-25 03:23:17 +0500

    Update splash_screen.java

--- app/src/main/java/com/test/kaarigar/splash_screen.java
+++ app/src/main/java/com/test/kaarigar/splash_screen.java
@@ -1,6 +1,9 @@
 package com.test.kaarigar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.window.SplashScreen;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,14 @@ public class splash_screen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intentHome = new Intent(splash_screen.this,MainActivity.class);
+                startActivity(intentHome);
+            }
+        },4000);
+
     }
 }

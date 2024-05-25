a6d6888418bc6c9ed3a8705881feb448f8cc4ae4
Happy <153811269+Muhtashim99@users.noreply.github.com>
2024-05-25 05:35:21 +0500

    oboarding done!

--- app/src/main/java/com/test/kaarigar/Signup.java
+++ app/src/main/java/com/test/kaarigar/Signup.java
@@ -1,12 +1,6 @@
 package com.test.kaarigar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.view.View;
-import android.view.View.OnClickListener;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Onboarding extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_onboarding);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-        Button next1 = findViewById(R.id.onbbtn);
-        next1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent isign = new Intent(Onboarding.this,Signup.class);
-                startActivity(isign);
-            }
-        });
-
-
-
     }
-
-
 }

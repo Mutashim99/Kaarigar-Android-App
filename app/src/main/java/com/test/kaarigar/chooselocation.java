0ccd26dcf132d97e28c2ef9b8271c60dffe4ff6c
Happy <153811269+Muhtashim99@users.noreply.github.com>
2024-05-25 17:14:21 +0500

    added chooselocation activity

--- app/src/main/java/com/test/kaarigar/chooselocation.java
+++ app/src/main/java/com/test/kaarigar/chooselocation.java
@@ -1,10 +1,6 @@
 package com.test.kaarigar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Signup extends AppCompatActivity {
+public class chooselocation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_chooselocation);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        TextView alreadyacc = findViewById(R.id.alreadyacc);
-        alreadyacc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent ialreadyacc = new Intent(Signup.this,Login.class);
-                startActivity(ialreadyacc);
-                finish();
-            }
-        });
-
-
     }
 }

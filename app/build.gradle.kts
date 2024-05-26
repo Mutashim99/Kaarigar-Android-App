35ceaed16e188539236cee556ff5d203be43d833
Happy <153811269+Muhtashim99@users.noreply.github.com>
2024-05-27 01:21:37 +0500

    fixed bottom nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
+    implementation ("com.google.android.material:material:1.4.0")
+
 }
--- app/src/main/java/com/test/kaarigar/MainActivity.java
+++ app/src/main/java/com/test/kaarigar/MainActivity.java
@@ -1,14 +1,14 @@
 package com.test.kaarigar;
 
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,16 +17,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+        // Handling window insets specifically for SearchView
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.searchView), (view, insets) -> {
+            WindowInsetsCompat insetsCompat = insets;
+            int topInset = insetsCompat.getInsets(WindowInsetsCompat.Type.systemBars()).top;
+            view.setPadding(view.getPaddingLeft(), topInset, view.getPaddingRight(), view.getPaddingBottom());
             return insets;
         });
 
-        List<SlideModel> imageList = new ArrayList<>();
+        // Set BottomNavigationView height if necessary
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        int bottomNavHeight = getResources().getDimensionPixelSize(R.dimen.bottom_navigation_height);
+        bottomNavigationView.getLayoutParams().height = bottomNavHeight;
+        bottomNavigationView.requestLayout();
 
+        // Setup image slider
+        List<SlideModel> imageList = new ArrayList<>();
         imageList.add(new SlideModel(R.drawable.banner1, ScaleTypes.FIT));
         imageList.add(new SlideModel(R.drawable.banner2, ScaleTypes.FIT));
         imageList.add(new SlideModel(R.drawable.banner3, ScaleTypes.FIT));
@@ -35,10 +43,5 @@ public class MainActivity extends AppCompatActivity {
 
         ImageSlider imageSlider = findViewById(R.id.image_slider);
         imageSlider.setImageList(imageList, ScaleTypes.FIT);
-
-
-
-
-
     }
 }

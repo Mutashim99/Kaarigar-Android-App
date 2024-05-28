298b1d1bba74ea97fdfdc6b46d0aff2f33214ba8
Happy <153811269+Muhtashim99@users.noreply.github.com>
2024-05-28 15:41:14 +0500

    done with homefrag

--- app/src/main/java/com/test/kaarigar/HomeFragment.java
+++ app/src/main/java/com/test/kaarigar/HomeFragment.java
@@ -1,60 +1,62 @@
 package com.test.kaarigar;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private RecyclerView recyclerView;
+    private CardAdapter cardAdapter;
+    private ImageSlider imageSlider;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Setup RecyclerView
+        recyclerView = view.findViewById(R.id.recyclerView);
+        LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(horizontalLayoutManager);
+
+        List<CardItem> cardItemList = new ArrayList<>();
+        cardItemList.add(new CardItem(R.drawable.pop1, "Home \nCleaning", "5.0", "Rs 1000"));
+        cardItemList.add(new CardItem(R.drawable.pop2, "Home \nDisInfection", "4.5", "Rs 2000"));
+        cardItemList.add(new CardItem(R.drawable.pop3, "Ac \nCheckup", "4.8", "Rs 1500"));
+        cardItemList.add(new CardItem(R.drawable.pop4, "Ac \nRepair", "4.2", "Rs 5000"));
+        cardItemList.add(new CardItem(R.drawable.pop5, "Ac \nMaintenance", "4.2", "Rs 1800"));
+
+        cardAdapter = new CardAdapter(cardItemList);
+        recyclerView.setAdapter(cardAdapter);
+
+        // Setup ImageSlider
+        imageSlider = view.findViewById(R.id.image_slider);
+
+        List<SlideModel> imageList = new ArrayList<>();
+        imageList.add(new SlideModel(R.drawable.banner1, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner2, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner3, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner4, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.banner5, ScaleTypes.FIT));
+
+        imageSlider.setImageList(imageList, ScaleTypes.FIT);
+
+        return view;
     }
 }
--- app/src/main/java/com/test/kaarigar/MainActivity.java
+++ app/src/main/java/com/test/kaarigar/MainActivity.java
@@ -4,17 +4,10 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import com.denzcoskun.imageslider.ImageSlider;
-import com.denzcoskun.imageslider.constants.ScaleTypes;
-import com.denzcoskun.imageslider.models.SlideModel;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import java.util.ArrayList;
-import java.util.List;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,30 +37,5 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
         bottomNav.getLayoutParams().height = bottomNavHeight;
         bottomNav.requestLayout();
-
-        // Setup image slider
-        List<SlideModel> imageList = new ArrayList<>();
-        imageList.add(new SlideModel(R.drawable.banner1, ScaleTypes.FIT));
-        imageList.add(new SlideModel(R.drawable.banner2, ScaleTypes.FIT));
-        imageList.add(new SlideModel(R.drawable.banner3, ScaleTypes.FIT));
-        imageList.add(new SlideModel(R.drawable.banner4, ScaleTypes.FIT));
-        imageList.add(new SlideModel(R.drawable.banner5, ScaleTypes.FIT));
-
-        ImageSlider imageSlider = findViewById(R.id.image_slider);
-        imageSlider.setImageList(imageList, ScaleTypes.FIT);
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        LinearLayoutManager horizontalLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(horizontalLayoutManager);
-
-        List<CardItem> cardItemList = new ArrayList<>();
-        cardItemList.add(new CardItem(R.drawable.pop1, "Home \nCleaning", "5.0", "Rs 1000"));
-        cardItemList.add(new CardItem(R.drawable.pop2, "Home \nDisInfection", "4.5", "Rs 2000"));
-        cardItemList.add(new CardItem(R.drawable.pop3, "Ac \nCheckup", "4.8", "Rs 1500"));
-        cardItemList.add(new CardItem(R.drawable.pop4, "Ac \nRepair", "4.2", "Rs 5000"));
-        cardItemList.add(new CardItem(R.drawable.pop5, "Ac \nMaintenance", "4.2", "Rs 1800"));
-
-        CardAdapter cardAdapter = new CardAdapter(cardItemList);
-        recyclerView.setAdapter(cardAdapter);
     }
 }

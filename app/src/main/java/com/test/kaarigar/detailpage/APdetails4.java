dc7d98bc8f773abc98e24ec2484b561a6d9391e2
Happy <153811269+Muhtashim99@users.noreply.github.com>
2024-06-06 00:43:14 +0500

    done with AP details

--- app/src/main/java/com/test/kaarigar/detailpage/APdetails4.java
+++ app/src/main/java/com/test/kaarigar/detailpage/APdetails4.java
@@ -1,12 +1,17 @@
 package com.test.kaarigar.detailpage;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.test.kaarigar.ConfirmBooking;
 import com.test.kaarigar.R;
 
 public class APdetails4 extends AppCompatActivity {
@@ -20,6 +25,16 @@ public class APdetails4 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button gonextact = findViewById(R.id.booknow);
+
+        gonextact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goproceed = new Intent(APdetails4.this , ConfirmBooking.class);
+                startActivity(goproceed);
+            }
         });
     }
 }

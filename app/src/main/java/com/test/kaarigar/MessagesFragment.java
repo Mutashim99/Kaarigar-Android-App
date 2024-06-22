0b53902fd81f73a4f152579dba71783f6c10bc79
Happy <153811269+Muhtashim99@users.noreply.github.com>
2024-06-22 16:35:01 +0500

    Setting up user profle page

--- app/src/main/java/com/test/kaarigar/MessagesFragment.java
+++ app/src/main/java/com/test/kaarigar/MessagesFragment.java
@@ -1,47 +1,45 @@
 package com.test.kaarigar;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MessagesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.io.IOException;
+
 public class MessagesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final int PICK_IMAGE = 1;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageView profilePic;
+    private TextView username, email, location;
+    private EditText password;
+    private Button resetPassword, changeProfilePic;
 
     public MessagesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MessagesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MessagesFragment newInstance(String param1, String param2) {
         MessagesFragment fragment = new MessagesFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("param1", param1);
+        args.putString("param2", param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,15 +48,69 @@ public class MessagesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            // Retrieve the parameters passed to the fragment
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_messages, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        profilePic = view.findViewById(R.id.profile_pic);
+        username = view.findViewById(R.id.username);
+        email = view.findViewById(R.id.email);
+        password = view.findViewById(R.id.password);
+        location = view.findViewById(R.id.location);
+        resetPassword = view.findViewById(R.id.reset_password);
+        changeProfilePic = view.findViewById(R.id.change_profile_pic);
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        username.setText(sharedPreferences.getString("username", "N/A"));
+        email.setText(sharedPreferences.getString("email", "N/A"));
+        password.setText(sharedPreferences.getString("password", ""));
+        location.setText(sharedPreferences.getString("location", "N/A"));
+
+        // Load saved profile picture if available
+        String profilePicUri = sharedPreferences.getString("profile_pic", null);
+        if (profilePicUri != null) {
+            profilePic.setImageURI(Uri.parse(profilePicUri));
+        }
+
+        changeProfilePic.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, PICK_IMAGE);
+        });
+
+        resetPassword.setOnClickListener(v -> {
+            // Logic to reset password
+            Toast.makeText(getActivity(), "Reset Password clicked", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == getActivity().RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                profilePic.setImageBitmap(bitmap);
+
+                // Save the profile picture Uri in SharedPreferences
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("profile_pic", imageUri.toString());
+                editor.apply();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/com/test/kaarigar/chooselocation.java
+++ app/src/main/java/com/test/kaarigar/chooselocation.java
@@ -1,50 +1,47 @@
 package com.test.kaarigar;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class chooselocation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chooselocation);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        String[] loclist = {"Karachi" , "Islamabad" , "Lahore" , "Peshawar" , "Quetta" , "Balochistan" ,"Kashmir" , "Murree" , "Rawalpindi" };
+        String[] loclist = {"Karachi", "Islamabad", "Lahore", "Peshawar", "Quetta", "Balochistan", "Kashmir", "Murree", "Rawalpindi"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, loclist);
         AutoCompleteTextView autoCompleteTextView = findViewById(R.id.listoflocations);
         autoCompleteTextView.setAdapter(adapter);
 
-        Button gotomain =findViewById(R.id.Finish);
+        Button gotomain = findViewById(R.id.Finish);
 
-        gotomain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent gomain = new Intent(chooselocation.this,Login.class);
+        gotomain.setOnClickListener(v -> {
+            String selectedLocation = autoCompleteTextView.getText().toString();
+            if (!selectedLocation.isEmpty()) {
+                // Save the selected location using SharedPreferences
+                SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("location", selectedLocation);
+                editor.apply();
+
+                // Redirect to the login activity
+                Intent gomain = new Intent(chooselocation.this, Login.class);
                 startActivity(gomain);
                 finish();
-
-
+            } else {
+                Toast.makeText(chooselocation.this, "Please select a location", Toast.LENGTH_SHORT).show();
             }
         });
-
-
     }
 }
